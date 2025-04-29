6b885b80cfe239c9d804de9f48a8d66c40a5f1c3
dreamingsushi <zhekinglim@gmail.com>
2025-04-29 12:20:55 +0800

    Attack combo

--- Assets/PlayerAttack.cs
+++ Assets/PlayerAttack.cs
@@ -5,23 +5,31 @@ using System.Collections;
 
 public class PlayerAttack : MonoBehaviourPunCallbacks
 {
-    private Animator animator;
     public bool isAttacking = false;
+    public bool canAttack = true;
 
-    [SerializeField] private float attackDuration = 0.5f; // How long attack lasts
+    [SerializeField] private float attackDuration = 0.5f;
+    [SerializeField] private float attackCooldown = 0.5f;
 
-    void Awake()
-    {
-        if (!photonView.IsMine) return;
-        animator = GetComponent<Animator>();
-    }
+    [Header("Combo Settings")]
+    [SerializeField] private int maxCombo = 3;
+    [SerializeField] private float comboResetTime = 1.0f;
+
+    private int currentCombo = 0;
+    private float lastAttackTime;
 
     public void OnAttack(InputAction.CallbackContext context)
     {
         if (!photonView.IsMine) return;
 
-        if (context.performed && !isAttacking)
+        if (context.performed && canAttack)
         {
+            // Check if within combo window
+            if (Time.time - lastAttackTime > comboResetTime)
+            {
+                currentCombo = 0; // Reset combo if too slow
+            }
+
             StartCoroutine(AttackRoutine());
         }
     }
@@ -29,9 +37,24 @@ public class PlayerAttack : MonoBehaviourPunCallbacks
     private IEnumerator AttackRoutine()
     {
         isAttacking = true;
+        canAttack = false;
+
+        // Do attack logic here (play animation based on currentCombo)
+        Debug.Log("Attack Combo #" + (currentCombo + 1));
+
+        lastAttackTime = Time.time;
+        currentCombo++;
+        if (currentCombo >= maxCombo)
+        {
+            currentCombo = 0; // Restart after max combo
+        }
 
         yield return new WaitForSeconds(attackDuration);
 
         isAttacking = false;
+
+        yield return new WaitForSeconds(attackCooldown);
+
+        canAttack = true;
     }
 }
