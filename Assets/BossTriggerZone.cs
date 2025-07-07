ff4fabf20f64850ddadf280ca485e69045c241c3
dreamingsushi <130603426+dreamingsushi@users.noreply.github.com>
2025-07-07 16:42:30 +0800

    CINEMATICS AND UI

--- Assets/BossTriggerZone.cs
+++ Assets/BossTriggerZone.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class BossTriggerZone : MonoBehaviour
 {
     public Boss bossScript; // Drag your boss GameObject here in Inspector
+    public GameObject bossTimeline;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             bossScript.StartPhase1();
+            bossTimeline.SetActive(true);
         }
     }
 
